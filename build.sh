469074e275505e3ca3cf91be153f0927f2667a93
Hleb Rubanau <g.rubanau@gmail.com>
2021-02-22 19:39:44 +0300

    fixup

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # this is a convenience script for local generation of the build artifacts
 set -e 
-set -o -pipefail 
+set -o pipefail 
 
 #docker run --rm -it -v $PWD:$PWD -w $PWD gradle:jdk11 ./gradlew build
 docker run --rm -it -v $PWD:$PWD -w $PWD gradle:jdk8 ./gradlew build
